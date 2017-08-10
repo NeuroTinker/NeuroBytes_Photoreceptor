989a8fdeac25a19cc94145b1d13e12a5b0f78a6e
jrwhite <jrwhite20@gmail.com>
2017-08-10 08:49:59 -0500

    added .fingerprint

--- FIRMWARE/src/main.c
+++ FIRMWARE/src/main.c
@@ -21,11 +21,10 @@
 #define SEND_PING_TIME		2000
 #define BUTTON_PRESS_TIME	2
 
-static uint32_t fingerprint[4] __attribute__((section (".fingerprint"))) __attribute__ ((__used__)) = {
+static uint32_t fingerprint[3] __attribute__((section (".fingerprint"))) __attribute__ ((__used__)) = {
 	2,
 	1,
-	5,
-	1
+	0
 };
 
 int temp, output;
